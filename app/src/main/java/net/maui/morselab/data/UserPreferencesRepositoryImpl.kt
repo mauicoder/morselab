790cf8c5da50131ce00343294bc406315fa0cd17
Maurizio Lo Bosco <maurizio.lobosco@raiffeisen.ch>
2024-09-17 10:29:10 +0200

    set default values for the user properties

--- app/src/main/java/net/maui/morselab/data/UserPreferencesRepositoryImpl.kt
+++ app/src/main/java/net/maui/morselab/data/UserPreferencesRepositoryImpl.kt
@@ -1,8 +1,12 @@
 package net.maui.morselab.data
 
+import android.util.Log
 import androidx.datastore.core.DataStore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 import net.maui.morselab.datastore.UserPreferences
+import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -13,30 +17,46 @@ class UserPreferencesRepositoryImpl @Inject constructor(
 
     private val TAG: String = "UserPreferencesRepository"
 
-    fun getPreferencesFlow() : Flow<UserPreferences> {
-        return userPreferencesStore.data
+    fun getPreferencesFlow(): Flow<UserPreferences> {
+        return getData()
     }
-/*
-    fun getData() = userPreferencesStore.data.catch { exception ->
+
+    private fun getData() = userPreferencesStore.data.catch { exception ->
         // dataStore.data throws an IOException when an error is encountered when reading data
         if (exception is IOException) {
             Log.e(TAG, "Error reading sort order preferences.", exception)
-            emit(UserPreferences.getDefaultInstance())
+            emit(defaultPreferences())
         } else {
-            throw exception
+            Log.e(TAG, "Other exception:", exception)
+            emit(defaultPreferences())
         }
-    }*/
+    }.map {
+        if (it.frequency != 0 && it.wpm != 0 && it.farnsworthWpm != 0)
+            it
+        else {
+            val builder = UserPreferences.newBuilder()
+            if (it.frequency == 0) builder.setFrequency(800) else builder.setFrequency(it.frequency)
+            if (it.wpm == 0) builder.setWpm(20) else builder.setWpm(it.wpm)
+            if (it.farnsworthWpm == 0) builder.setFarnsworthWpm(20) else builder.setFarnsworthWpm(it.farnsworthWpm)
+            builder.build()
+        }
+    }
+
+    private fun defaultPreferences(): UserPreferences =
+        UserPreferences.newBuilder().setFrequency(800).setWpm(20).setFarnsworthWpm(20).build()
 
     override suspend fun updateFrequency(frequency: Int) {
         userPreferencesStore.updateData { preferences ->
             preferences.toBuilder().setFrequency(frequency).build()
         }
     }
+
     override suspend fun updateWpm(wpm: Int) {
         userPreferencesStore.updateData { preferences ->
             preferences.toBuilder().setWpm(wpm).build()
         }
     }
+
     override suspend fun updateFarnsworthWpm(farnsworthWpm: Int) {
         userPreferencesStore.updateData { preferences ->
             preferences.toBuilder().setFarnsworthWpm(farnsworthWpm).build()
---
+++ app/src/test/java/net/maui/morselab/viewmodel/UserPreferencesViewModelTest.kt
@@ -0,0 +1,32 @@
+package net.maui.morselab.viewmodel
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class UserPreferencesViewModelTest {
+
+    @Test
+    fun increaseFrequency() {
+    }
+
+    @Test
+    fun decreaseFrequency() {
+    }
+
+    @Test
+    fun increaseWpm() {
+    }
+
+    @Test
+    fun decreaseWpm() {
+    }
+
+    @Test
+    fun increaseFarnsworthWpm() {
+    }
+
+    @Test
+    fun decreaseFarnsworthWpm() {
+    }
+}
